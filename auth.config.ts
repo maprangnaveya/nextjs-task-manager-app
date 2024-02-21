3fd65a8994cbb7e027dbc5f89b89ffdcf6cc74de
Naveya Jintaya <naveya@divertise.asis>
2024-02-22 03:24:30 +0700

    Automatically redirect anonymous user to login page.

--- auth.config.ts
+++ auth.config.ts
@@ -4,5 +4,21 @@ export const authConfig = {
     pages: {
         signIn: '/login',
     },
+    callbacks: {
+        authorized({ auth, request: { nextUrl } }) {
+
+            const isLoggedIn = !!auth?.user;
+            const isOnLoginPage = nextUrl.pathname.startsWith('/login');
+            if (!isOnLoginPage) {
+                if (isLoggedIn) return true;
+                return false; // Redirect unauthenticated users to login page
+            } else if (isLoggedIn) {
+                let newUrl = new URL('/', nextUrl);
+                console.log("!!! newUrl: ", newUrl);
+                return Response.redirect(newUrl);
+            }
+            return true;
+        },
+    },
     providers: [], // Add providers with an empty array for now
 } satisfies NextAuthConfig;
