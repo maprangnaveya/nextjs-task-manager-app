1153384af8ee59b76befa9052e6f450f78d85bdf
Naveya Jintaya <naveya@divertise.asis>
2024-02-22 03:37:16 +0700

    Update color in default error page.

--- app/error.tsx
+++ app/error.tsx
@@ -14,7 +14,7 @@ export default function Error({
       <h2 className="text-center">Something went wrong!</h2>
       <h3 className="text-center">{error.message}</h3>
       <button
-        className="mt-4 rounded-md bg-blue-500 px-4 py-2 text-sm text-white transition-colors hover:bg-blue-400"
+        className="mt-4 rounded-md bg-violet-500 px-4 py-2 text-sm text-white transition-colors hover:bg-violet-400"
         onClick={
           // Attempt to recover by trying to re-render the invoices route
           () => reset()
