25d3fadbeb26382721a3e3448d90d570500ec5eb
Naveya Jintaya <naveya@divertise.asis>
2024-02-21 15:00:34 +0700

    Add TaskTabs and TaskList in the main page.

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,6 @@
 import Image from 'next/image';
+import TaskTabs from './ui/main/task-tabs';
+import TaskList from './ui/main/task-list';
 
 // TODO: Get user full name
 let userFullName = 'Sherlock';
@@ -11,14 +13,12 @@ export default function Page() {
         <br />
         Are you ready to finish your task?
       </div>
-      <div className="w:full mt-4 flex grow flex-col items-center justify-center gap-4">
-        <div className="flex flex-col   gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
-          <p className={`text-xl text-gray-800 md:text-3xl md:leading-normal`}>
-            Tab component
-          </p>
+      <div className="mt-4 flex w-full grow flex-col items-center justify-center gap-4">
+        <div className="flex flex-col   gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-3/5 md:px-20">
+          <TaskTabs />
         </div>
-        <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
-          Task component
+        <div className="relative flex w-full">
+          <TaskList />
         </div>
       </div>
     </main>
