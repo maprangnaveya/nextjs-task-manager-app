71faaf59f474e679477d1e3d0fec727f72ace33b
Naveya Jintaya <naveya@divertise.asis>
2024-02-22 02:37:44 +0700

    Update login form to login by username instead of email.

--- app/ui/login-form.tsx
+++ app/ui/login-form.tsx
@@ -1,16 +1,15 @@
 import { roboto } from '@/app/ui/fonts';
 import {
-  AtSymbolIcon,
+  UserIcon,
   KeyIcon,
   ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
-import { Button } from './button';
 
 export default function LoginForm() {
   return (
     <form className="space-y-3">
-      <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+      <div className="flex-1 items-center justify-center rounded-lg bg-white px-6 pb-4 pt-8">
         <h1 className={`${roboto.className} mb-3 text-2xl`}>
           Please log in to continue.
         </h1>
@@ -18,20 +17,20 @@ export default function LoginForm() {
           <div>
             <label
               className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-              htmlFor="email"
+              htmlFor="username"
             >
-              Email
+              Username
             </label>
             <div className="relative">
               <input
                 className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                id="email"
-                type="email"
-                name="email"
-                placeholder="Enter your email address"
+                id="username"
+                type="username"
+                name="username"
+                placeholder="Enter your username"
                 required
               />
-              <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+              <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
             </div>
           </div>
           <div className="mt-4">
@@ -66,8 +65,13 @@ export default function LoginForm() {
 
 function LoginButton() {
   return (
-    <Button className="mt-4 w-full">
+    <button
+      className={
+        'mt-4 flex h-10 w-1/2 items-center rounded-lg bg-violet-500 px-4 text-sm font-medium text-white  aria-disabled:cursor-not-allowed aria-disabled:opacity-50' +
+        'transition-colors hover:bg-violet-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-violet-500 active:bg-violet-600'
+      }
+    >
       Log in <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" />
-    </Button>
+    </button>
   );
 }
