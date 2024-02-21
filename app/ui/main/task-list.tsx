df0725d8b42fd692eb95acbe2b52a521eb50f8c3
Naveya Jintaya <naveya@divertise.asis>
2024-02-21 15:08:51 +0700

    Move task card to a new file `task-card`.

--- app/ui/main/task-list.tsx
+++ app/ui/main/task-list.tsx
@@ -1,9 +1,7 @@
-import Table from '@/app/ui/invoices/table';
-import { CreateInvoice } from '@/app/ui/invoices/buttons';
 import { lusitana } from '@/app/ui/fonts';
 import { InvoicesTableSkeleton } from '@/app/ui/skeletons';
 import { Suspense } from 'react';
-import { Card } from '../dashboard/cards';
+import { Card } from './task-card';
 
 export default async function TaskList({
   searchParams,
