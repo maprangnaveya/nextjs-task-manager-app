c02dc151894f6919b5401c4473c27fd88a3af0ee
Naveya Jintaya <naveya@divertise.asis>
2024-02-22 21:31:30 +0700

    Adjust hero layout.

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -4,7 +4,7 @@ export const Hero = async () => {
   const session = await auth();
   console.log('!!! session: ', session);
   return (
-    <div className="h-10 px-6 py-20">
+    <div className="h-fit p-2 md:h-10  md:p-6 lg:py-20">
       <h1 className=" text-4xl font-bold">
         Hello There, {session?.user?.name || '...'}
       </h1>
