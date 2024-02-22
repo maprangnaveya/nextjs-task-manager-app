e7e221bce12405b83d78f71320ae334b133704dd
Naveya Jintaya <naveya@divertise.asis>
2024-02-22 21:35:18 +0700

    Adjust height of hero.

--- app/ui/hero.tsx
+++ app/ui/hero.tsx
@@ -4,8 +4,8 @@ export const Hero = async () => {
   const session = await auth();
   console.log('!!! session: ', session);
   return (
-    <div className="h-fit p-2 md:h-10  md:p-6 lg:py-20">
-      <h1 className=" text-4xl font-bold">
+    <div className="h-fit p-2 md:p-6">
+      <h1 className="text-4xl font-bold">
         Hello There, {session?.user?.name || '...'}
       </h1>
       <p>Have a nice day!</p>
